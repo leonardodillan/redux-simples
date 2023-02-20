9fe6fa605d0f30dc57053e8c26bab5d0cf95b0f3
Leonardo Dillan <ldbraz@stefanini.com>
2023-02-19 21:21:28 -0300

    upload

---
+++ src/store/reducers/numeros.js
@@ -0,0 +1,22 @@
+const initalState = {
+    min: 1,
+    max: 10
+}
+
+export default function(state = initalState, action) {
+    switch(action.type) {
+        case 'NUM_MIN_ALTERADO':
+            return {
+                ...state,
+                min: action.payload
+            }
+        case 'NUM_MAX_ALTERADO':
+            return {
+                ...state,
+                max: action.paylaod
+            }
+        default:
+            return state
+    }
+    
+}
